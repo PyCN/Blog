13adcfbff9f74d438158d42c96638beea2c22995
caitinggui <1029645297@qq.com>
2016-09-13 21:49:55 +0800

    完成优化views

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.views.generic.edit import CreateView, FormView
 from django.contrib import auth
 from django.template.context import RequestContext
 from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+#from django.utils.decorators import method_decorator
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
@@ -162,16 +162,20 @@ class LoginView(FormView):
             auth.login(self.request, user)
             self.request.session['username'] = username
             return HttpResponseRedirect('/')
-            return render_to_response('blog/index.html', RequestContext(self.request, {'username':username}))
+            # 在模板中处理的session的用户信息，所以不用返回具体的user
+            # return render_to_response('blog/index.html', RequestContext(self.request, {'username':username}))
         else:
             login_info = "Username or password is error"
             return render(self.request, 'blog/login.html', {'form': form,'login_info':login_info})
+            # render is beautiful
             return render_to_response('blog/login.html', RequestContext(self.request, {'form': form,'login_info':login_info}))
     
     def form_invalid(self, form):
         login_info = 'input error'
         return render_to_response('blog/login.html', RequestContext(self.request, {'form': form, 'login_info':login_info}))
+       
         
+# 使用LoginView，此处函数不生效，仅供参考      
 def login(request):
     login_info = ''
     if request.method == 'GET':
@@ -194,6 +198,7 @@ def login(request):
             login_info = 'input error'
             return render_to_response('blog/login.html', RequestContext(request, {'form': form, 'login_info':login_info}))
             
+            
 def regist(request):
     regist_info = ''
     if request.method == 'GET':
@@ -201,7 +206,7 @@ def regist(request):
         # context, content, contents are the same(变量名不影响使用)
         contents = {'form':form}
         
-        # those are the same
+        # thise are the same
         return render(request, 'blog/regist.html', contents)
         return render(request, 'blog/regist.html', {'form':form})
         return render_to_response('blog/regist.html', RequestContext(request, {'form':form}))
@@ -214,13 +219,13 @@ def regist(request):
             phone     = form.cleaned_data['phone']
             if password1 == password2:
                 user_filter_result = User.objects.filter(username=username) 
-                if len(user_filter_result)>0:  
+                if user_filter_result:  
                     regist_info = "用户名已存在"
                     return render_to_response("blog/regist.html", RequestContext(request,{'form':form, 'regist_info':regist_info}))  
                 else:
                     user = User.objects.create_user(username=username,password=password1)
-                    #user.is_active=True  
-                    user.save
+                    # user.is_active=True  
+                    # user.save
                     user_profile = UserProfile()
                     user_profile.user_id = user.id
                     user_profile.phone = phone
@@ -291,7 +296,6 @@ def generate_qrcode(request):
         except:
             return HttpResponseRedirect('/')            
             
-        # url_data = request.POST.get('name')
         if url_data == "if leave empty, it will be current url":
             img = qrcode.make(request.get_host()+request.path)
         else:
@@ -304,8 +308,5 @@ def generate_qrcode(request):
         return response
     else:
         return HttpResponseRedirect('/')            
-#class Login(FormView):
-#    template_name = 'blog/login.html'
+
      
-class Regist(FormView):
-    template_name = 'blog/regist.html'
