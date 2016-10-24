c5380f8df1d681499671d080ba1aa60b6ed9a23c
caitinggui <1029645297@qq.com>
2016-10-24 23:03:29 +0800

    添加完整的文件

---
+++ haystack/search_indexes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from blog.models import Article
+from haystack import indexes
+
+
+class ArticleIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+
+    def get_model(self):
+        return Article
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
---
+++ haystack/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url  
+from haystack.views import SearchView  
+
+urlpatterns = [  
+    url(r'^$', SearchView(), name='haystack_search'),  
+]  
