72cf1012d3a1481fcdb832e576e973864218034a
caitinggui <1029645297@qq.com>
2016-11-15 23:38:28 +0800

    优化search结果页面

--- blog/search_views.py
+++ blog/search_views.py
@@ -8,12 +8,4 @@ class MySearchView(SearchView):
 
     def extra_context(self):
         context = super(MySearchView, self).extra_context()
-        context.update({'first':'first'})
         return context
-    
-    def get_queryset(self):
-        queryset = super(MySearchView, self).get_queryset()
-        with open('/home/ctg/django.txt', 'w') as f:
-            f.write(str(queryset))
-            f.write(dir(queryset))
-        return ''
