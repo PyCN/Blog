64b4702ea77a33d4e5fe070e39689cb076930c11
caitinggui <1029645297@qq.com>
2016-11-16 00:05:52 +0800

    修改search模板

--- blog/search_views.py
+++ blog/search_views.py
@@ -3,9 +3,19 @@
 
 from haystack.views import SearchView
 
+from blog.models import Article, Category, Tag, BlogComment, UserProfile
+
 
 class MySearchView(SearchView):
 
+    def __call__(self, request):
+        self.template = 'blog/index.html'
+        super(MySearchView, self).__call__(request)
+        return self.create_response()
+
     def extra_context(self):
         context = super(MySearchView, self).extra_context()
+        context['category_list'] = Category.objects.all()
+        context['date_archive'] = Article.objects.archive()
+        context['tag_list'] = Tag.objects.all()
         return context
