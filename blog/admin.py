e7bb2fc5c9a41e6ce67d089f9827d7394524758f
caitinggui <1029645297@qq.com>
2016-12-27 00:55:33 +0800

    添加权限表到admin

--- blog/admin.py
+++ blog/admin.py
@@ -7,4 +7,6 @@ admin.site.register(Article)
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(BlogComment)
+admin.site.register(Permission)
+admin.site.register(UserProfile)
 
