0a40b774d5a581b90ead0880574519692f896be7
caitinggui <1029645297@qq.com>
2017-02-26 16:46:15 +0800

    修改项目sys路径

--- blog_project/wsgi.py
+++ blog_project/wsgi.py
@@ -13,7 +13,7 @@ from os.path import join,dirname,abspath
 from django.core.wsgi import get_wsgi_application
  
 PROJECT_DIR = dirname(dirname(abspath(__file__)))
-sys.path.insert(0,PROJECT_DIR)
+sys.path.append(PROJECT_DIR)
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "blog_project.settings" 
  
