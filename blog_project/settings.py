f20d1ee77469e2a27dd9ad3b0e8bdd08c5db722a
caitinggui <1029645297@qq.com>
2017-03-24 21:43:10 +0800

    [fix]debug runserver error

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -107,7 +107,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'blog_project.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
