7bbfba8ff0f3d9a73fbdcba407666b4b6ef7cf89
caitinggui <1029645297@qq.com>
2017-06-20 11:16:25 +0800

    remove logging because wsgi.py will start logging

--- manage.py
+++ manage.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
-import logging
-import logging.config
 
-
-from configs.log_config import log_setting
-
-logging.config.dictConfig(log_setting)
-logger = logging.getLogger('web')
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "configs.settings")
 
-    logger.info('Web start!')
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
