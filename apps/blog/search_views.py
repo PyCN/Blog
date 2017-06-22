2b6099b4ee3999e21dbb020546f4893d3d5964f7
caitinggui <1029645297@qq.com>
2017-06-22 17:00:08 +0800

    add exception

--- apps/blog/search_views.py
+++ apps/blog/search_views.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import logging
+
 from haystack.views import SearchView
+from django.shortcuts import Http404
 
 from blog.models import Article, Category, Tag, BlogComment, UserProfile,\
     VisitorIP
 from blog.views import get_context_data_all
 
 
+logger = logging.getLogger('web')
+
+
 class MySearchView(SearchView):
 
     def __call__(self, request):
         self.template = 'blog/index.html'
-        super(MySearchView, self).__call__(request)
-        return self.create_response()
+        try:
+            super(MySearchView, self).__call__(request)
+            result = self.create_response()
+        except Exception, e:
+            logger.error('search error: %s', e)
+            raise Http404
+        return result
 
     def extra_context(self):
         context = super(MySearchView, self).extra_context()
